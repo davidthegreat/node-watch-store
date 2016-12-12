ad228cda244603b8c8693d2e67245c5660d8a426
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-12-12 15:07:16 -0800

    add router for registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
 
+var csrfProtection = csrf();
+router.use(csrfProtection);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* GET users registration listing. */
+router.get('/register', function(req, res, next) {
+  res.render('user/signup',{csrfToken: req.csrfToken()});
+});
+
+
 
 module.exports = router;
