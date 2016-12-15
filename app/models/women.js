c7cae7b8f5b59173fc598188b5260bc3edb8b613
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-12-15 02:38:54 -0800

    add women watch model

---
+++ app/models/women.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+
+
+var schema = new Schema({
+	imagePath: {type: String, require: true},
+	secondimagePath: {type: String, require: true},
+	thirdimagePath: {type: String, require: true},
+	title: {type: String, require: true},
+	description: {type: String, require: true},
+	price: {type: Number, require: true},
+	sale : {type: String, require: true}
+});
+
+module.exports = mongoose.model('Women', schema);
