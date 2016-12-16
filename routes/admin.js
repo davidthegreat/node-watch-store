083767315b5823b31ecd14e85550c332dcb9d204
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-12-16 11:35:07 -0800

    add a function to check if current user is an admin and add this function to admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', isAdmin, function(req, res, next) {
   res.render('admin/index', { title: 'Express',layout: false});
 });
 
 module.exports = router;
+
+function isAdmin(req, res, next) {
+   if (req.user && req.user.isAdmin === true){
+     next();
+   }
+   else{
+     res.send(401, 'Unauthorized');
+   }
+ }
