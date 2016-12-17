be92a850cae99ea3d23e5cc8ae5767b5d32f827e
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-12-16 23:21:06 -0800

    add all the orders to the admin main page

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
+var Men = require('../app/models/men');
+var Cart = require('../app/models/cart');
+var Order = require('../app/models/order');
+var Kid = require('../app/models/kid');
+var Women = require('../app/models/women');
+
 /* GET home page. */
 router.get('/', isAdmin, function(req, res, next) {
-  res.render('admin/index', { title: 'Express',layout: false});
+  Order.find( function(err, orders) {
+    if (err) {
+        return res.write('Error!');
+    }
+    var cart;
+    orders.forEach(function(order) {
+        cart = new Cart(order.cart);
+        order.items = cart.generateArray();
+    });
+    res.render('admin/index', { orders: orders,layout: false });
+});
 });
 
 module.exports = router;
