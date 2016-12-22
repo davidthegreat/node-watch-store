1a368acdd2d79b0eef4323055a4499a8a9b95acd
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-12-22 13:36:29 -0800

    add gif and gif javascripts

---
+++ public/javascripts/load.js
@@ -0,0 +1,10 @@
+jQuery(function($){
+
+
+
+   jQuery(window).load(function() { // makes sure the whole site is loaded
+      jQuery('#aa-preloader-area').delay(300).fadeOut('slow'); // will fade out
+    })
+
+
+});
