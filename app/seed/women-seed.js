94861f41e81ac6f9a8532d3c9bf44b9c33dbf58d
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-03-16 02:19:12 -0700

    Add the stars for product seed

--- app/seed/women-seed.js
+++ app/seed/women-seed.js
@@ -12,6 +12,11 @@ var womens =  [
     description: "A bold pink dial houses easy-to-read markers and a convenient calendar date on this fine Michael Kors watch. Crafted with yellow goldtone stainless steel, this timepiece features a push-button clasp and polished finish.This feminine fashion statement and functional timepiece stylishly transitions from casual day wear into evening wear with ease.",
     price: 179,
     sale: 'show',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'show',
+    starfive: 'show'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/7569470/Bulova-Womens-96R153-Precisionist-Stainless-Steel-Diamond-Bezel-Watch-23f55f73-5092-4a94-9e8a-3f2980127b94_600.jpeg',
@@ -21,6 +26,11 @@ var womens =  [
     description: "A twirling, diamond-encrusted bezel surrounds the mother of pearl centerpiece dial to style this Precisionist watch from Bulova. This beautiful accessory is finished with a classic, multi-link stainless steel bracelet. ",
     price: 139,
     sale: 'none',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'show',
+    starfive: 'show'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/7972139/Movado-Womens-2100006-Collection-Yellow-Goldplated-Swiss-Quartz-Watch-f9ac70b1-379c-4bbf-a7a2-91f4db1ec3c2_600.jpg',
@@ -30,6 +40,11 @@ var womens =  [
     description: "Lend an elegant air to any outfit with this sophisticated watch from Movado. The signature Movado dot and goldtone watch hands provide a polished accent on the smooth black dial. With a rich texture, the black leather strap proves a lush complement to the sleek gold-plated stainless steel case.",
     price: 224,
     sale: 'none',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'show',
+    starfive: 'hide'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/10277428/SO-CO-New-York-Womens-SoHo-Crystal-Cut-Dial-Stainless-Steel-Mesh-Watch-f075c583-36d8-48e9-9b83-32369b78ecb2_600.jpg',
@@ -48,6 +63,11 @@ var womens =  [
     description: "Silver-tone stainless steel case and bracelet with white center links. Crystal accented bezel. White/grey dial with rose gold-tone hands and crystal index hour markers. Roman numerals mark the quarter hour positions. Quartz movement. Minera crystal. Push/pull crown. Case: diameter 30mm thickness 9mm. Band: width 7mm length 7.25in. Pushbutton clasp. Water resistant up to 50 meters/165 feet. Functions: hour, minute, second. Fossil Women's ES3962 Virginia White/Grey Dial Two-Tone Stainless Steel Bracelet Watch.All measurements are approximate and may vary slightly from the listed dimensions.Women's watch bands can be sized to fit 6.5-inch to 7.5-inch wrists.",
     price: 300,
     sale: 'none',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'hide',
+    starfive: 'hide'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/16363011_ORIGINAL_0_600.jpg',
@@ -57,6 +77,11 @@ var womens =  [
     description: "Exemplifying elegance and sophistication, this watch and its stainless steel construction ensures a durable and gorgeous addition to any wrist. In the center of the dial sits the Michael Kors logo to truly showcase your obvious fashion sense. No matter the occasion or ensemble, this rose gold watch will complement it effortlessly.",
     price: 169,
     sale: 'none',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'show',
+    starfive: 'show'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/8685344/Michael-Kors-Womens-MK2256-Runaway-Slim-Double-Leather-Watch-0d4b396b-3564-44b2-9b34-3757ddf38a28_600.jpg',
@@ -75,6 +100,11 @@ var womens =  [
     description: "Add a slim accent to your wrist with this gorgeous design from Seiko. The gold and silver-tone stainless steel bracelet and case accent the black dial with gold-tone embellishments. Quartz movement. Water resistant to 30 meters. This elegant women's watch is perfect for everyday or dressy occasions.",
     price: 80,
     sale: 'show',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'hide',
+    starfive: 'hide'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/2564415/Geneva-Womens-Platinum-Polished-Bangle-Watch-1279b107-bbf6-49d1-9497-e860edc4902a_600.jpg',
@@ -84,6 +114,11 @@ var womens =  [
     description: "A gorgeous combination of function and beauty, this bangle watch looks like a piece of jewelry while keeping the time with excellent Japanese quartz precision. With a round silver-plated case and a graceful hinge cuff, the watch sits elegantly on your wrist. Available in three combinations of black or silver dials and goldtone or silvertone cuffs, the watch gives you more or less chromatic contrast, according to your preferences.",
     price: 40,
     sale: 'show',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'hide',
+    starfour: 'hide',
+    starfive: 'hide'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/6293772/Burgi-Womens-Crystal-Mesh-Bracelet-Quartz-Watch-c2b38b81-a2f8-4003-9830-199cbcd3e931_600.jpg',
@@ -93,6 +128,11 @@ var womens =  [
     description: "This exceptional, Burgi women's, dazzling, crystal, quartz watch is perfect for any occasion. The bezel features genuine crystals with a sunray and flower pattern dial. ",
     price: 45,
     sale: 'show',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'show',
+    starfive: 'hide'
   }),
   new Women({
     imagePath: 'https://ak1.ostkcdn.com/images/products/8700606/Michael-Kors-Womens-MK3192-Darci-Rose-Goldtone-Stainless-Steel-Watch-713723a5-169e-4e2b-8289-4ca8c165cfde_600.jpg',
@@ -111,6 +151,11 @@ var womens =  [
     description: "Capture every moment in time through the sapphire window of this Movado women's watch. A rich black dial contrasts with silvertone hands to create visual depth, and the analog hands move gracefully to and from the concave dot centered at the 12 o'clock position. The sleek and elegant push-/pull crown makes setting this timepiece simple.",
     price: 229,
     sale: 'none',
+    starone: 'show',
+    startwo: 'show',
+    starthree: 'show',
+    starfour: 'show',
+    starfive: 'show'
   }),
   // new Women({
   //   imagePath: 'http://localhost:3000/images/GenevaPlatinumFaux.jpg',
