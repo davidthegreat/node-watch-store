55688a2658e9c44e94c4e0f203098a0db5751845
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-03-16 02:16:57 -0700

    update the models for stars

--- app/models/men.js
+++ app/models/men.js
@@ -9,7 +9,12 @@ var schema = new Schema({
 	title: {type: String, require: true},
 	description: {type: String, require: true},
 	price: {type: Number, require: true},
-	sale : {type: String, require: true}
+	sale : {type: String, require: true},
+	starone: {type: String, require: true},
+	startwo: {type: String, require: true},
+	starthree: {type: String, require: true},
+	starfour: {type: String, require: true},
+	starfive: {type: String, require: true}
 });
 
 module.exports = mongoose.model('Men', schema);
